0be50c2f2f048c8df710a9afce48840848faba97
dreamNawaporn <96407096+dreamNawaporn@users.noreply.github.com>
2023-03-16 10:28:33 +0700

    Initial commit

---
+++ model/condb.php
@@ -0,0 +1,26 @@
+<?php {
+    class ConDB
+    {
+        private $host, $db, $user, $pass;
+        public $conn;
+
+        public function ConDB()
+        {
+            $this->host = 'localhost';
+            $this->user = 'root';
+            $this->pass = '';
+            $this->db = 'termfootball';
+        }
+
+        public function connect()
+        {
+            try {
+                $this->conn = new PDO("mysql:host=localhost;dbname=termfootball", "root", "");
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+    }
+?>
+
+<?php }
